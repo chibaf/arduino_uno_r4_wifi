df4dafba3859399df01c06fd144e097dcc364d36
chibaf <chibaf@mac.com>
2026-02-28 20:30:15 +0900

    Add files via upload

--- read_wifi.py
+++ read_wifi.py
@@ -1,9 +1,9 @@
 import socket
-UDP_IP = "192.168.0.9"  
-UDP_PORT = 139 
+UDP_IP = "192.168.0.5"  
+UDP_PORT = 5005 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("192.168.0.9",139))
+sock.bind((UDP_IP,UDP_PORT))
 while True:
     data, addr = sock.recvfrom(2)  
     print(data)
